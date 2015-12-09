7ae9cc08fd82fe725c21e93a97864f06de155dd0
Joe Kantor <joekantor01@yahoo.com>
2015-12-09 09:44:44 -0600

    W02D02

--- w02/d02/starter/js/main.js
+++ w02/d02/starter/js/main.js
@@ -2,3 +2,61 @@
 // One way to get input is to use the prompt() command
 // You can have your adventure play out using alerts or in the console using console.log().
 
+var bridge = prompt("It seems the bridge will need some repairs before we can cross it, let’s try using some of the planks we brought with us to repair the bridge, but how many should we use? We have a total of 6 planks with us. Be careful to not bring too many or you may collapse the bridge due to the extra weight, too few and the bridge can’t support itself and collapse!");
+
+	if (bridge === 3) {
+		prompt("Perfect! Just the right mixture of planks and weight to serve as a manageable bridge to cross, the bridge collapses as you and your team cross it. Let’s hope that this path leads outside, forward ho! But what’s that in the distance? A dead end? Well, you lived a nice long adventuring life. Time to spend the rest of it deciding who amongst your team who you’ll have to eat first.");
+	}
+
+	if else (bridge >4) {
+		prompt("Oh no, you brought too many planks! The weight causes the bridge to collapse and you fall to your doom! That really does suck, you’re the worst adventurer ever!);
+	}
+
+var vines = prompt("How should we proceed through these vines, we can ignore them and walk through them, chop them away, or burn them away.");
+	
+	if (vines === I) 
+		option = prompt("Oh no! These are venomous snakes, not vines! What kind of adventurer are you, confusing vines with snakes. It’s like you’re not even trying! Now look at that, you’ve gone and died to the venom.");
+	if (vines === C)
+		option = prompt()
+
+	if (vines === B)
+		option = prompt()
+
+var well_lit_path = prompt("It would appear that the floor of the pathway down the well lit path is covered in various marking on all of the stones, let’s figure out how we want to proceed down the path!");
+
+	if (===)
+
+	if (===)
+
+	if (===)
+
+My my, we’ve discovered a hidden temple hidden deep within the Amazon rainforest, time to explore it with our team. Let’s rally them together and head inside. Hmm, but which path do we take? There seems to be separate paths.
+	-The path with the rickety bridge (‘b’)
+		-It seems the bridge will need some repairs before we can cross it, let’s try using some 		of the planks we brought with us to repair the bridge, but how many should we use? We 		have a total of 6 planks with us. Be careful to not bring too many or you may collapse 		the bridge due to the extra weight, too few and the bridge can’t support itself and 		collapse! 
+			-(value <2) -oh no, not enough planks! The bridge collapses and you fall to your doom. What a bummer.
+			-(value >4) oh no, you brought too many planks! The weight causes the bridge to collapse and you fall to your doom! That really does 					suck, you’re the worst adventurer ever!
+			-(value = 3) Perfect! Just the right mixture of planks and weight to serve as a manageable bridge to cross, the bridge collapses as you 					and your team cross it. Let’s hope that this path leads outside, 					forward ho! But what’s that in the distance? A dead end? Well, you 					lived a nice long adventuring life. Time to spend the rest of it 					deciding who amongst your team you’ll have to eat first.
+			
+	-The path covered in vines (‘v’)
+		-How should we proceed through these vines, we can ignore them and walk through them, chop them away, or burn them away.
+			-‘i’ gnore
+				-oh no! These are venomous snakes, not vines! What kind of adventurer are you, confusing vines with snakes. It’s like you’re not even trying! Now 				look at that, you’ve gone and died to the venom.
+			-‘c’ hop
+				-It would appear that the snakes you are chopping are not vines as you had initially thought. Maybe you should look a little closer at the details 				before just diving into a situation. Too little too late though, you die to a 				venomous bite.
+			-‘b’ urn
+				-Now you’ve gone and done it, the entire temple is on fire because you forgot that fire has a tendency to spread. How do we escape the inferno?
+					-‘r’ un through it
+						-well, you ran through the inferno and right into a giant pitfall trap, you and your entire team are stuck at the bottom of a 1						00 foot hole with no means of escape, good luck in the 						drawing of the straws to decide who you’ll eat first to attempt 						to survive!
+					’t’ urn back
+						-you manage to escape and live another day, mark that temple on your map and remember to come back with 						proper gear next time!
+					-‘w’ ait it out
+						-turns out, you’re not fireproof and you run out of oxygen in the enclosed temple. Maybe instead of thinking like a 						charcoal briquette, you should be thinking like an adventure.
+	-The well lit path (‘li’)
+		-It would appear that the floor of the pathway down the well lit path is covered in various marking on all of the stones, let’s figure out how we want to proceed down the path!
+			-’n’ umbers
+				-As soon as you and your team begin to step on stones with a number on them, poisonous darts begin to shoot out of the wall, you and your team 				are all killed. Maybe you should have tested the stone first.
+			-‘l’ etters
+				-The moment your foot touches the stone with the letter, the entire temple begins to collapse. You and your team die in the cave-in. No snarky 				explanation here, I didn’t even see that coming and I wrote this story.
+			‘bl’ ank stones
+				-You take a step onto the blank stone, and wait for something to happen but nothing does. You call for your team to proceed forward. When your 				team fills the pathway, a giant flat stone falls to cover the entrance, You 				hear a clunk and a giant stone ball falls from the ceiling and barrels 				towards your team. You call for your team to run further into the pathway 				but are met with a dead end. You tried, but like most decision making, 				you’ll always be stuck between a rock and a hard place.
+				
