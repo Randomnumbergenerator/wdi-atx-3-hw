38b02c862a9811c15114c7bf0d637bbb67d837e5
Joe Kantor <joekantor01@yahoo.com>
2016-01-06 08:34:29 -0600

    homework submit git issues

--- w04/d02/starter/word.rb
+++ w04/d02/starter/word.rb
@@ -1,9 +1,33 @@
 class Word
+
   def initialize(original_word)
     @original_word = original_word
+    @vowels = 
   end
+
+  original_word = gets.chomp
+
+  def to_s
+
 
   def original_word
     return @original_word.downcase
   end
+
+  def initialize (consonants)
+  	@
+  end
+
+
 end
+
+vowels = ["a", "e", "i", "o", "u"]
+alphabet = %w("a"..."z")
+consonants = alphabet - vowels
+
+def piglatinize
+	if @original_word.consonants
+		puts
+
+	elsif @original_word.vowels
+		puts
